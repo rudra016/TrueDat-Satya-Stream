a11817a76b55be6b94024309e2b0d09029bdb3af
aarushiksk <aarushee002@gmail.com>
2025-01-02 17:53:20 +0530

    added files

--- serper_check.py
+++ serper_check.py
@@ -7,6 +7,6 @@ load_dotenv()
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 def serper_check(text : str):
-    search = GoogleSerperAPIWrapper()
-    response = requests.get(f"https://api.serper.dev/search?q={text}")
-    return response.json()
+    search = GoogleSerperAPIWrapper(type="news")
+    return search.results(text)
+
