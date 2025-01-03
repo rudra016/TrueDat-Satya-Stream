52cfbf7f90c4b37a29596fecbeab5d86fbcb399e
aarushiksk <aarushee002@gmail.com>
2025-01-03 20:23:38 +0530

    deleted paddlepaddle

--- Backend/check_paddle.py
+++
@@ -1,7 +0,0 @@
-from paddleocr import PaddleOCR
-
-ocr = PaddleOCR(use_angle_cls=True)  # Initialize the model
-results = ocr.ocr('testing2.jpeg', cls=True)
-
-for line in results[0]:
-    print(f"Detected text: {line[1][0]}, Confidence: {line[1][1]}")
